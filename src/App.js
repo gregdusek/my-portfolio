52a83b5f793f027e1764ae84235e35208f6c1ea1
GDusek <70817559+GDusek@users.noreply.github.com>
2021-03-10 16:02:35 -0600

    Rename component file names to index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import React from 'react';
-import About from '/Components/About';
-import Contact from '/Components/Contact';
-import Experience from '/Components/Experience';
-import Home from '/Components/Home';
-import Landing from '/Components/Landing';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Experience from './Components/Experience';
+import Home from './Components/Home';
+import Landing from './Components/Landing';
 import Navigate from './Components/Navbar';
 import Portfolio from './Components/Portfolio';
 import Skills from './Components/Skills';
