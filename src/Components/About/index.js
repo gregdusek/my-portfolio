5d873a6e6802c17c4aeddf2277355f60f2bb0942
GDusek <70817559+GDusek@users.noreply.github.com>
2021-03-15 17:23:48 -0500

    Added className to divs

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,1 +1,13 @@
+import './styles.css';
 
+const About = () => {
+    return (
+        <div className='about-container'>
+            <h1 className='about-title'>ABOUT ME</h1>
+            <p className='about-para'>Hey, I'm Greg Dusek</p>
+
+        </div>
+    )
+}
+
+export default About;
