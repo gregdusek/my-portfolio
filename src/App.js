1785d45a172e686f6f5601a3990ef0049f47fa8c
GDusek <70817559+GDusek@users.noreply.github.com>
2021-03-15 17:38:10 -0500

    Removed react bootstrap & added react router
    Refactored file for react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Experience from './Components/Experience';
 import Home from './Components/Home';
-import Landing from './Components/Landing';
+// import Landing from './Components/Landing';
 import Navigate from './Components/Navbar';
 import Portfolio from './Components/Portfolio';
 import Skills from './Components/Skills';
@@ -14,14 +15,16 @@ function App() {
   return (
     <div className="App">
       <Navigate />
-      {/* <Home /> */}
-      <About />
-      {/* <Experience /> */}
-      {/* <Skills /> */}
-      {/* <Landing /> */}
-      {/* <Portfolio /> */}
-      {/* <Toolbox /> */}
-      {/* <Contact /> */}
+      <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/About' exact component={About} />
+          <Route path='/Experience' exact component={Experience} />
+          <Route path='/Skills' exact component={Skills} />
+          {/* <Landing /> */}
+          <Route path='/Portfolio' exact component={Portfolio} />
+          <Route path='/Toolbox' exact component={Toolbox} />
+          <Route path='/Contact' exact component={Contact} />
+      </Switch>
     </div>
   );
 }
