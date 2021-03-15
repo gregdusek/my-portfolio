ee36ccf0b057ad327912eeeb60270ed88d66fec0
GDusek <70817559+GDusek@users.noreply.github.com>
2021-03-15 13:20:43 -0500

    Tested font, didn't change anything

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -3,13 +3,13 @@ import './styles.css';
 const Navigate = () => {
     return (
         <div className="navbar">
-            <div className="nav-link" href="#home">Home</div>
-            <div className="nav-link" href="#about">About Me</div>
-            <div className="nav-link" href="#skills">Skills</div>
-            <div className="nav-link" href="#experience">Experience</div>
-            <div className="nav-link" href="#portfolio">Portfolio</div>
-            <div className="nav-link" href="#toolbox">Toolbox</div>
-            <div className="nav-link" href="#contact">Contact</div>
+            <div className="nav-link" href="#home">HOME</div>
+            <div className="nav-link" href="#about">ABOUT ME</div>
+            <div className="nav-link" href="#skills">SKILLS</div>
+            <div className="nav-link" href="#experience">EXPERIENCE</div>
+            <div className="nav-link" href="#portfolio">PORTFOLIO</div>
+            <div className="nav-link" href="#toolbox">TOOLBOX</div>
+            <div className="nav-link" href="#contact">CONTACT</div>
         </div>
     )
 }
