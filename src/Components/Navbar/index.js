47336dab407e95dd154b82b2bd74b8c90f7cdcc4
GDusek <70817559+GDusek@users.noreply.github.com>
2021-03-15 12:07:26 -0500

    Add basic nav links

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,17 @@
+import './styles.css';
+
+const Navigate = () => {
+    return (
+        <div className="navbar">
+            <div className="nav-link" href="#home">Home</div>
+            <div className="nav-link" href="#about">About Me</div>
+            <div className="nav-link" href="#skills">Skills</div>
+            <div className="nav-link" href="#experience">Experience</div>
+            <div className="nav-link" href="#portfolio">Portfolio</div>
+            <div className="nav-link" href="#toolbox">Toolbox</div>
+            <div className="nav-link" href="#contact">Contact</div>
+        </div>
+    )
+}
+
+export default Navigate;
