1c649782767956badd59c2daeb705e79eada3bd8
GDusek <70817559+GDusek@users.noreply.github.com>
2021-03-16 17:09:35 -0500

    Created skills container layout

--- src/Components/Skills/index.js
+++ src/Components/Skills/index.js
@@ -3,9 +3,25 @@ import './styles.css';
 const Skills = () => {
 
     return (
-
-        <div>
-
+        <div className='skills-container'>
+            <h1 className='page-title'>SKILLS</h1>
+            <div className='column-container'>
+                <div className='column-language'>
+                    <h1 className='column-title'>Languages</h1>
+                </div>
+                <div className='column-frontend'>
+                    <h1 className='column-title'>Frontend</h1>
+                </div>
+                <div className='column-backend'>
+                    <h1 className='column-title'>Backend</h1>
+                </div>
+                <div className='column-databases'>
+                    <h1 className='column-title'>Databases</h1>
+                </div>
+                <div className='column-tools'>
+                    <h1 className='column-title'>Tools</h1>
+                </div>
+            </div>
         </div>
     )
 };
