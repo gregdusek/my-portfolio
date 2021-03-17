e6fbd82cb30d90bec521636308f8569347e28d64
GDusek <70817559+GDusek@users.noreply.github.com>
2021-03-17 04:41:31 -0500

    Setup skills page layout with content

--- src/Components/Skills/index.js
+++ src/Components/Skills/index.js
@@ -1,25 +1,126 @@
 import './styles.css';
 
 const Skills = () => {
-
     return (
         <div className='skills-container'>
             <h1 className='page-title'>SKILLS</h1>
             <div className='column-container'>
                 <div className='column-language'>
                     <h1 className='column-title'>Languages</h1>
+                        <div className='language-container'>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957750/Dev%20Language%20Icons/html5_lmrgt0.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957747/Dev%20Language%20Icons/css3_p0ssge.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957750/Dev%20Language%20Icons/javascript_db9xwo.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957742/Dev%20Language%20Icons/ruby_pzhglj.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <div className='icon' src='' alt='doc'/>
+                            </div>
+                            <div className='icon-container'>
+                                <div className='icon' src='' alt='doc'/>
+                            </div>
+                        </div>
                 </div>
                 <div className='column-frontend'>
                     <h1 className='column-title'>Frontend</h1>
+                        <div className='frontend-container'>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957741/Dev%20Language%20Icons/react_xjy6ou.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957736/Dev%20Language%20Icons/tailwind_ne3bz3.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957746/Dev%20Language%20Icons/bootstrap_p7eeq8.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957743/Dev%20Language%20Icons/sass_jx4obe.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957738/Dev%20Language%20Icons/materialui_qylajm.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957742/Dev%20Language%20Icons/redux_mrfmvf.svg' alt='svg'/>
+                            </div>
+                        </div>
                 </div>
                 <div className='column-backend'>
                     <h1 className='column-title'>Backend</h1>
+                    <div className='backend-container'>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957743/Dev%20Language%20Icons/nodejs-1_jtj3is.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957749/Dev%20Language%20Icons/express_tvwfwe.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957742/Dev%20Language%20Icons/ruby_pzhglj.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957738/Dev%20Language%20Icons/rails-1_p4kc3n.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <div className='icon' src='' alt='svg'/>
+
+                            </div>
+                            <div className='icon-container'>
+                                <div className='icon' src='' alt='svg'/>
+
+                            </div>
+                        </div>
                 </div>
                 <div className='column-databases'>
-                    <h1 className='column-title'>Databases</h1>
+                    <h1 className='column-title'>Databases/Hosting</h1>
+                    <div className='databases-container'>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957739/Dev%20Language%20Icons/postgresql_ltbseu.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957738/Dev%20Language%20Icons/mysql_cdf60q.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957738/Dev%20Language%20Icons/mongodb_ednvig.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957745/Dev%20Language%20Icons/digitalocean_ulmslp.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957745/Dev%20Language%20Icons/amazonwebservice_skpi40.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615973097/Dev%20Language%20Icons/Netlify-Logo.wine_dlteet.svg' alt='svg'/>
+                            </div>
+                        </div>
                 </div>
                 <div className='column-tools'>
                     <h1 className='column-title'>Tools</h1>
+                    <div className='tools-container'>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615973500/Dev%20Language%20Icons/postman-logo-stacked_wgz311.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957740/Dev%20Language%20Icons/vs_code_mxps5g.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957748/Dev%20Language%20Icons/github_i7cwek.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957741/Dev%20Language%20Icons/slack-2_s1ihss.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957740/Dev%20Language%20Icons/zoom1_fyqnyr.svg' alt='svg'/>
+                            </div>
+                            <div className='icon-container'>
+                                <img className='icon' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957740/Dev%20Language%20Icons/discord_ygs0ff.svg' alt='svg'/>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
