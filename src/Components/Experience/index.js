e86a44fc6ac382462fbe1ec3f30039d1c2404987
GDusek <70817559+GDusek@users.noreply.github.com>
2021-03-22 23:57:48 -0500

    Added experience formatting

--- src/Components/Experience/index.js
+++ src/Components/Experience/index.js
@@ -1,10 +1,39 @@
 import './styles.css';
 
 const Experience = () => {
-
     return (
-        <div>
-
+        <div className='experience-container'>
+            <h1 className='page-title'>EXPERIENCE</h1>
+                <div className='row-container'>
+                    <div className='training-row'>
+                        <h2 className='training-title'>Training</h2>
+                        <p className='training-paragraph'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias omnis delectus, architecto ipsam odio sunt maxime esse, molestias quos pariatur, explicabo ab maiores! Illum exercitationem distinctio vitae quam, ipsam amet tempora illo pariatur accusantium blanditiis nisi. Temporibus assumenda hic numquam!
+                        </p>
+                    </div>
+                    <div className='background-row'>
+                        <h2 className='background-title'>Professional</h2>
+                        <p className='background-paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptatibus accusantium eaque quo autem tempora consectetur dignissimos vel consequuntur praesentium aliquid, laboriosam architecto fugiat at ex necessitatibus nam sequi sit quas veritatis! Minus rerum eligendi et voluptatibus, odio modi cum.
+                        </p>
+                    </div>
+                    <div className='column-container'>
+                        <div className='column-1'>
+                            <p className='column-paragraph1'>I am a full stack software engineer, software developer, web developer, and the man behind the code. My interest is full stack, frontend, backend, web development, and software development.
+                            </p>
+                            <h1 className='column-title'>HEY, I'M GREG!</h1>
+                        </div>
+                        <div className='column-2'>
+                            <p className='column-paragraph1'>I am a full stack software engineer, software developer, web developer, and the man behind the code. My interest is full stack, frontend, backend, web development, and software development.
+                            </p>
+                            <h1 className='column-title'>HEY, I'M GREG!</h1>
+                        </div>
+                        <div className='column-3'>
+                            <p className='column-paragraph1'>I am a full stack software engineer, software developer, web developer, and the man behind the code. My interest is full stack, frontend, backend, web development, and software development.
+                            </p>
+                            <h1 className='column-title'>HEY, I'M GREG!</h1>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 };
