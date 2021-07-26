ab7504bd68dbaf568e9af8928b72d791ee21ee65
GDusek <70817559+GDusek@users.noreply.github.com>
2021-07-26 18:09:02 -0500

    Changed link titles to undercase

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 const Navigate = () => {
     return (
         <div className="navbar">
-            <Link className="nav-link" to='/'>HOME</Link>
-            <Link className="nav-link" to='/About'>ABOUT ME</Link>
-            <Link className="nav-link" to='/Skills'>SKILLS</Link>
-            <Link className="nav-link" to='/Experience'>EXPERIENCE</Link>
-            <Link className="nav-link" to='/Portfolio'>PORTFOLIO</Link>
-            <Link className="nav-link" to='/Contact'>CONTACT</Link>
+            <Link className="nav-link" to='/'>Home</Link>
+            <Link className="nav-link" to='/About'>About Me</Link>
+            <Link className="nav-link" to='/Skills'>Skills</Link>
+            <Link className="nav-link" to='/Experience'>Experience</Link>
+            <Link className="nav-link" to='/Portfolio'>Portfolio</Link>
+            <Link className="nav-link" to='/Contact'>Contact</Link>
         </div>
     )
 }
