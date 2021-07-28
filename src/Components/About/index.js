1e9dae10c908144de6a38bb68f34eca516994a6a
GDusek <70817559+GDusek@users.noreply.github.com>
2021-07-27 21:58:32 -0500

    Removed hobbies and paragraph title section
    Rewrote 'about me' paragraph

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -5,37 +5,22 @@ const About = () => {
         <div className='page-container'>
             <h1 className='page-title'>About Me</h1>
             <div className='aboutColumn-container'>
-                <div className='about-1'>
-                    <h1 className='aboutLeft-title'>HEY, I'M GREG!</h1>
-                    <p className='column-paragraph1'>I am a full stack software engineer, web developer, and the man behind the code. I am trained in both frontend and backend development. I can help build applications with responsive design, databases from the ground up, and custom API's.
+                <div className='about'>
+                    <p className='column-paragraph'>Hey, I'm Greg! Thanks for taking the time to visit my dev portfolio. I am a full stack software engineer and the man behind the code. Being trained in both frontend and backend development, I can help build applications with responsive design, databases, and custom API's from the ground up.
                     <br/><br/>
-                    Born in a small town and raised in a big city, at a young age I learned how to thrive in a fast paced environment while maintaining my southern charm. Experience from a variety of career fields has taught me valuable skills leading to my success.
+                    Born in a small town and raised in a big city, I was forced to become independent at a young age, where I learned how to thrive in a fast paced environment while maintaining my southern charm. Being raised by parents who instilled good morals and values within me, I learned about working hard to achieve my goals. Experience from a variety of career fields has taught me valuable skills leading the success in my professional career. Success comes from a strong support system and my wife has been my biggest supporter. Honestly, I can say with confidence that I would not have gotten where I am, or had the success I've had; if it weren't for her support. Every day is a blessing to have someone who constantly encourages daily and challenges me to achieve my goals. 
                     <br/><br/>
-                    Let's talk more about how I can help build your e-commerce site, blog, personal portfolio, or custom software. Please see my contact page for more info.
-                    <br/><br/></p>
-                    
-                </div>
-                <div className='about-2'>
-                    <img className='about-img' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957747/Dev%20Language%20Icons/CWP-271_sized_av7zgj.jpg' alt='' /> 
-                </div>
-                <div className='about-3'>
-                <h1 className='aboutRight-title'>MY HOBBIES & INTERESTS</h1>
-                    <p className='column-paragraph2'>When I'm not behind the keyboard working, you can find me keeping busy with a variety of hobbies. 
+                    Since this is an "about me" section in a portfolio, I won't go through my resume however, I do have one available on the experience page. Previously, I mentioned working in a variety of career fields, each of which has been a key contributor to the skillset I've developed. Describing myself isn't my strong suite, most people don't enjoy talking about themselves but I feel the best way to describe me is a jack of all trades, master of none. Someone who's a master of their craft rarely talks about being a master because to them, they're constantly learning, and evolving. A friend once told me, "the world needs ditch diggers", and that resonated with me as I thought about it. He's right though, the world needs people who clock in to the bare minimum. Settling for the bare minimum has never worked for me and it's the biggest reason I make every effort to go above and beyond. I'm a strong communicator and I enjoy sharing my knowledge with others. I'm always looking for new ways to improve my knowledge and I'm always looking for new ways to improve my skillset. I'm always looking for new ways to improve my skillset. 
                     <br/><br/>
-                            <li>Enjoying the company of my wife</li>
-                            <li>Playing outside with my dogs</li>
-                            <li>Fly fishing</li>
-                            <li>Listening to live music</li>
-                            <li>Exploring the offroad trails in my jeep</li>
-                            <li>Camping</li>
-                            <li>Watching hockey and college football</li>
-                            <li>Studying the market for my next investment</li>
-                            <li>Running my barbecue smoker</li>
-                            <li>Bourbon tasting</li>
-                            <li>Spending time with friends and family</li>
-                            <li>Traveling to new places</li>
+                    I hope this is a useful resource for you. If you have any questions, comments, or concerns, please feel free to contact me. I'd love to hear from you. Let's talk more about how I can help build your e-commerce site, blog, personal portfolio, or custom software. Please see my contact page to find out how to get in touch with me.
+
+                    Kindest regards,
+                    <br/><br/>
+                            -Greg Dusek-
+                    <br/><br/>
                     </p>
                 </div>
+                    <img className='about-img' src='https://res.cloudinary.com/gregdusek/image/upload/v1615957747/Dev%20Language%20Icons/CWP-271_sized_av7zgj.jpg' alt='' /> 
             </div>
         </div>
     )
