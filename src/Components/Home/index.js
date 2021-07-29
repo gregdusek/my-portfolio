23d8674551ac23b1d5421d124abd8523d8c13c3f
GDusek <70817559+GDusek@users.noreply.github.com>
2021-07-29 00:22:29 -0500

    Populated home content

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,10 +1,27 @@
 import './styles.css';
+import Typed from 'react-typed';
+
+const words = ['Problem Solver','Leader','Multitasker','Team Player','Volunteer','Developer','Software Engineer'];
 
 const Home = () => {
 
     return (
-        <>
-        </>
+        <div className="page-container">
+            <h1 className="name">Greg Dusek</h1>
+            <div className="page-content">
+                <div className="typed">
+                    <h1>I am a...<Typed
+                                  strings={words}
+                                  typeSpeed={100}
+                                  backSpeed={125}
+                                  loop={true}/>
+                    </h1>
+                </div>
+                <div className="home-intro">
+                    <h2>An analytical full-stack software engineer proven to learn quickly, adapt to changes, and thrive in a fast-paced environment. Prior experience in the information technology and commercial audiovisual fields taught the necessary skills of leadership, time management, multitasking, communicating effectively in diverse environments, and solving problems efficiently. Motivated to learn and grow by leveraging skills to excel as a software engineer at your company.</h2>
+                </div>
+            </div>
+        </div>
     )
 };
 
